aa297f930d81d54055f2710befdfa18a0f5fd71f
lvxiaofei <2571200135@qq.com>
2020-11-05 15:09:26 +0800

    项目初始结构

---
+++ hos-stuinfo/src/main/java/com/lwy/hos/stuinfo/HosStuinfoApplication.java
@@ -0,0 +1,13 @@
+package com.lwy.hos.stuinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HosStuinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HosStuinfoApplication.class, args);
+	}
+
+}
---
+++ hos-stuinfo/src/test/java/com/lwy/hos/stuinfo/HosStuinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwy.hos.stuinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HosStuinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
